2d24077221b87e0c676510518fd3f8453a03d805
ZoeAcero <zar.amazona@gmail.com>
2024-04-25 13:00:47 +0200

    a

--- src/main/java/Poblacion_Bacteria/PoblacionBacteria.java
+++ src/main/java/Poblacion_Bacteria/PoblacionBacteria.java
@@ -1,12 +1,25 @@
 package Poblacion_Bacteria;
 
-import org.jdatepicker.impl.JDatePanelImpl;
-import org.jdatepicker.impl.JDatePickerImpl;
-import org.jdatepicker.impl.UtilDateModel;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.Properties;
 
-public class PoblacionBacteria {
+
+public class PoblacionBacteria extends JFrame{
+    private JTextField nameField;
+    private JTextField startDateField;
+    private JTextField endDateField;
+    private JTextField initialBacteriaCountField;
+    private JTextField temperatureField;
+    private JTextField luminosityField;
+    private JTextField initialFoodField;
+    private JTextField foodIncreaseDayField;
+    private JTextField foodIncreaseAmountField;
+    private JTextField finalFoodField;
+
+    public BacteriaPopulation() {
+        setLayout(new GridLayout(11, 2));
+
+        add(new JLabel("Name:"));
+        nameField = new JTextField();
+        add(nameField);
 }
