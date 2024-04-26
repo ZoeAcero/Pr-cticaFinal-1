e5e7410cd464bcf55ab3170b81a6893432b6b099
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 12:43:12 +0200

    a

--- src/main/java/Poblacion_Bacteria/BacteriaDAO.java
+++ src/main/java/Poblacion_Bacteria/BacteriaDAO.java
@@ -1,4 +1,16 @@
 package Poblacion_Bacteria;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BacteriaDAO {
-}
+    private final String url = "jdbc:mysql://localhost:3306/mydatabase";
+    private final String user = "username";
+    private final String password = "password";
+
+    public void saveBacteria(Bacteria bacteria) throws SQLException {
+        String sql = "INSERT INTO bacteria (name, start_date, end_date, initial_bacteria_count, temperature, luminosity, initial_food, food_increase_day, food_increase_amount, final_food) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    }
