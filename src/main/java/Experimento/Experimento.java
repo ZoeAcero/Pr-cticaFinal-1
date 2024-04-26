13494a241ff4fd1ab8c161804ec8e766d4e798f5
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 13:02:19 +0200

    a

--- src/main/java/Experimento/Experimento.java
+++ src/main/java/Experimento/Experimento.java
@@ -1,8 +1,18 @@
 package Experimento;
 
+import Poblacion_Bacteria.PoblacionBacteria;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Experimento {
-    
+    private List<PoblacionBacteria> populations;
+
+    public Experimento() {
+        populations = new ArrayList<>();
+    }
+
+    public void addPopulation(PoblacionBacteria poblacion) {
+        populations.add(poblacion);
+    }
 }
