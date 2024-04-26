56b3a310c5e7b0b337297e52f84afc902eef7f21
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 13:09:18 +0200

    a

--- src/main/java/Experimento/ManejoExperimentos.java
+++ src/main/java/Experimento/ManejoExperimentos.java
@@ -17,4 +17,17 @@ public class ManejoExperimentos {
             return experiment;
         }
     }
+
+    public void saveExperiment(Experimento experiment, String filename) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(experiment);
+            experimentos.put(filename, experiment);
+        }
+    }
+
+    public Experimento createNewExperiment(String filename) {
+        Experimento experiment = new Experimento();
+        experimentos.put(filename, experiment);
+        return experiment;
+    }
 }
