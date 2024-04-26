a765463675043caf057cfc48f08619664cbcbed0
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 13:08:27 +0200

    a

--- src/main/java/Experimento/ManejoExperimentos.java
+++ src/main/java/Experimento/ManejoExperimentos.java
@@ -5,4 +5,16 @@ import java.util.*;
 
 public class ManejoExperimentos {
     private Map<String, Experimento> experimentos;
+
+    public ManejoExperimentos() {
+        experimentos = new HashMap<>();
+    }
+
+    public Experimento openExperiment(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            Experimento experiment = (Experimento) ois.readObject();
+            experimentos.put(filename, experiment);
+            return experiment;
+        }
+    }
 }
