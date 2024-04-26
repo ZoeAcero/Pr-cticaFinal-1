68de07d99555117e452cd116d5e7ace07b9e2a02
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 12:43:36 +0200

    a

--- src/main/java/Poblacion_Bacteria/BacteriaDAO.java
+++ src/main/java/Poblacion_Bacteria/BacteriaDAO.java
@@ -13,4 +13,22 @@ public class BacteriaDAO {
     public void saveBacteria(Bacteria bacteria) throws SQLException {
         String sql = "INSERT INTO bacteria (name, start_date, end_date, initial_bacteria_count, temperature, luminosity, initial_food, food_increase_day, food_increase_amount, final_food) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, bacteria.getName());
+            stmt.setString(2, bacteria.getStartDate());
+            stmt.setString(3, bacteria.getEndDate());
+            stmt.setInt(4, bacteria.getInitialBacteriaCount());
+            stmt.setInt(5, bacteria.getTemperature());
+            stmt.setString(6, bacteria.getLuminosity());
+            stmt.setInt(7, bacteria.getInitialFood());
+            stmt.setInt(8, bacteria.getFoodIncreaseDay());
+            stmt.setInt(9, bacteria.getFoodIncreaseAmount());
+            stmt.setInt(10, bacteria.getFinalFood());
+
+            stmt.executeUpdate();
+        }
+    }
+
     }
