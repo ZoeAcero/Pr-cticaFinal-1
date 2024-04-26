7414c61511ad9d620cf185855d494ad88cec0689
ZoeAcero <zar.amazona@gmail.com>
2024-04-26 13:19:00 +0200

    a

--- src/main/java/Experimento/ManejoExperimentos.java
+++ src/main/java/Experimento/ManejoExperimentos.java
@@ -2,7 +2,6 @@ package Experimento;
 
 import java.io.*;
 import java.util.*;
-import Experimento.Experimento;
 
 public class ManejoExperimentos {
     private Map<String, Experimento> experimentos;
@@ -14,7 +13,7 @@ public class ManejoExperimentos {
     public Experimento openExperiment(String filename) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
             Experimento experiment = (Experimento) ois.readObject();
-            experimentos.put(filename, experimentos);
+            experimentos.put(filename, experiment);
             return experiment;
         }
     }
@@ -22,13 +21,13 @@ public class ManejoExperimentos {
     public void saveExperiment(Experimento experiment, String filename) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
             oos.writeObject(experiment);
-            experimentos.put(filename, experiment);
+            experimentos.put(filename, experiment); // Corrección aquí
         }
     }
 
     public Experimento createNewExperiment(String filename) {
         Experimento experiment = new Experimento();
-        experimentos.put(filename, experimentos);
+        experimentos.put(filename, experiment);
         return experiment;
     }
 
