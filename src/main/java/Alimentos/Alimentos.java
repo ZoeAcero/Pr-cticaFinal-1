1ad64cd8ea928ed23952c90916a9eae0fc506716
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 13:35:48 +0200

    s

--- src/main/java/Alimentos/Alimentos.java
+++ src/main/java/Alimentos/Alimentos.java
@@ -3,5 +3,8 @@ package Alimentos;
 import javax.swing.*;
 import java.awt.*;
 
-public class Alimentos {
+public class Alimentos extends JFrame{
+    private JTextField nombreField;
+    private JTextField cantidadNutrientesField;
+    private Alimentos alimentos;
 }
