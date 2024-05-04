4baa0af4208f0bb75801f73589c690ee12409428
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 13:37:25 +0200

    s

--- src/main/java/Alimentos/Alimentos.java
+++ src/main/java/Alimentos/Alimentos.java
@@ -7,4 +7,24 @@ public class Alimentos extends JFrame{
     private JTextField nombreField;
     private JTextField cantidadNutrientesField;
     private Alimentos alimentos;
+
+    public Alimentos() {
+        setLayout(new GridLayout(3, 2));
+
+        add(new JLabel("Nombre:"));
+        nombreField = new JTextField();
+        add(nombreField);
+
+        add(new JLabel("Cantidad de Nutrientes:"));
+        cantidadNutrientesField = new JTextField();
+        add(cantidadNutrientesField);
+
+        JButton button = new JButton("Guardar");
+        button.addActionListener(e -> {
+            String nombre = nombreField.getText();
+            double cantidadNutrientes = Double.parseDouble(cantidadNutrientesField.getText());
+            alimentos = new Alimentos(nombre, cantidadNutrientes);
+            JOptionPane.showMessageDialog(null, "Alimento guardado: " + alimento.getNombre());
+        });
+        }
 }
