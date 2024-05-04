607f157f1228c977c8de3ead93c761d6fde65d0d
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 14:06:49 +0200

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import Alimentos.*;
+import Experimento.*;
+import Archivo.*;
+import java.util.ArrayList;
+import java.util.List;
+import Poblacion_Bacteria.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ManejoExperimentos gestor = new ManejoExperimentos();
+
+        
+
+    }
