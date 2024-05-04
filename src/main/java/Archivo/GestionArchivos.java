154a3f0234f518a2b52a0486f5139baf1577042d
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 13:48:04 +0200

    a

--- src/main/java/Archivo/GestionArchivos.java
+++ src/main/java/Archivo/GestionArchivos.java
@@ -5,5 +5,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GestionArchivos extends JFrame{
-    
+    private JTextField rutaArchivoField;
+    private JTextArea experimentoArea;
+    private GestionArchivos gestor;
+
+    public GestionArchivos() {
+        setLayout(new GridLayout(3, 2));
+
+        add(new JLabel("Ruta del Archivo:"));
+        rutaArchivoField = new JTextField();
+        add(rutaArchivoField);
 }
