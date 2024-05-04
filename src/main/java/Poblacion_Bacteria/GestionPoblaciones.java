3a7c8ce28a909e8a0148b44eac3bf45afde6c2a0
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 12:17:56 +0200

    a

--- src/main/java/Poblacion_Bacteria/GestionPoblaciones.java
+++ src/main/java/Poblacion_Bacteria/GestionPoblaciones.java
@@ -5,5 +5,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GestionPoblaciones {
-}
+public class GestionPoblaciones  extends JFrame{
+    private GestionPoblaciones gestor;
+    private DefaultListModel<PoblacionBacteria> listModel;
+    private JList<PoblacionBacteria> list;
+
+    public GestionPoblaciones() {
+        gestor = new GestionPoblaciones();
+        listModel = new DefaultListModel<>();
+        list = new JList<>(listModel);
+
+        JButton addButton = new JButton("Add");
+
+    }
