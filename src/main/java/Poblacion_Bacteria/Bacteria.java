8898ff1aa0dd9186e7259172cbd0396866b6b18a
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 12:23:17 +0200

    a

---
+++ src/main/java/Poblacion_Bacteria/Bacteria.java
@@ -0,0 +1,101 @@
+package Poblacion_Bacteria;
+
+import java.io.Serializable;
+
+public class Bacteria implements Serializable{
+    private String name;
+    private String startDate;
+    private String endDate;
+    private int initialBacteriaCount;
+    private double temperature;
+    private double luminosity;
+    private double initialFood;
+    private int foodIncreaseDay;
+    private double foodIncreaseAmount;
+    private double finalFood;
+
+    public Bacteria(String name, String startDate, String endDate, int initialBacteriaCount, double temperature, double luminosity, double initialFood, int foodIncreaseDay, double foodIncreaseAmount, double finalFood) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.initialBacteriaCount = initialBacteriaCount;
+        this.temperature = temperature;
+        this.luminosity = luminosity;
+        this.initialFood = initialFood;
+        this.foodIncreaseDay = foodIncreaseDay;
+        this.foodIncreaseAmount = foodIncreaseAmount;
+        this.finalFood = finalFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public int getInitialBacteriaCount() {
+        return initialBacteriaCount;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getLuminosity() {
+        return luminosity;
+    }
+
+    public double getInitialFood() {
+        return initialFood;
+    }
+
+    public int getFoodIncreaseDay() {
+        return foodIncreaseDay;
+    }
+
+    public double getFoodIncreaseAmount() {
+        return foodIncreaseAmount;
+    }
+
+    public double getFinalFood() {
+        return finalFood;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setInitialBacteriaCount(int initialBacteriaCount) {
+        this.initialBacteriaCount = initialBacteriaCount;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setLuminosity(double luminosity) {
+        this.luminosity = luminosity;
+    }
+
+    public void setInitialFood(double initialFood) {
+        this.initialFood = initialFood;
+    }
+
+    public void setFoodIncreaseDay(int foodIncreaseDay) {
+        this.foodIncreaseDay = foodIncrease
+
+}
