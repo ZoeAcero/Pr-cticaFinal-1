1c4eaa2ddab8cbc4d8f410be949d0219bb2abc3f
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 13:42:39 +0200

    a

--- src/main/java/Alimentos/CalculadoraAlimentos.java
+++ src/main/java/Alimentos/CalculadoraAlimentos.java
@@ -8,4 +8,16 @@ public class CalculadoraAlimentos extends JFrame {
     private JTextField numeroBacteriasInicialesField;
     private JTextField resultadoField;
     private CalculadoraAlimentos calculador;
+
+    public CalculadoraAlimentos() {
+        setLayout(new GridLayout(3, 2));
+
+        add(new JLabel("Número de Bacterias Iniciales:"));
+        numeroBacteriasInicialesField = new JTextField();
+        add(numeroBacteriasInicialesField);
+
+        add(new JLabel("Resultado:"));
+        resultadoField = new JTextField();
+        resultadoField.setEditable(false);
+        add(resultadoField);
 }
