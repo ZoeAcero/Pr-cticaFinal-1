a146ae2e3ccc4b654d23c6d0104625aef4021ba5
ZoeAcero <zar.amazona@gmail.com>
2024-05-04 13:48:38 +0200

    a

--- src/main/java/Archivo/GestionArchivos.java
+++ src/main/java/Archivo/GestionArchivos.java
@@ -15,4 +15,17 @@ public class GestionArchivos extends JFrame{
         add(new JLabel("Ruta del Archivo:"));
         rutaArchivoField = new JTextField();
         add(rutaArchivoField);
+
+        add(new JLabel("Experimento:"));
+        experimentoArea = new JTextArea();
+        experimentoArea.setEditable(false);
+        add(new JScrollPane(experimentoArea));
+
+        JButton abrirButton = new JButton("Abrir");
+        abrirButton.addActionListener(e -> {
+            String rutaArchivo = rutaArchivoField.getText();
+            gestor = new GestionArchivos();
+            Experimento experimento = gestor.abrirArchivo(rutaArchivo);
+            experimentoArea.setText(experimento.toString());
+        });
 }
